719bb561c418fe207b5ec947fb9aa7e0a15b79ac
ghetter <49289242+ghetter@users.noreply.github.com>
2021-09-15 22:18:58 +0600

    my bad #1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.icon-menu').click(function(event){
+        $(this).toggleClass('active');
+        $('.menu__body').toggleClass('active');
+        $('.header__logo').toggleClass('active');
+        $('body').toggleClass('lock');
+});
+    $('.menu__link').click(function(event){
+        $('.icon-menu').toggleClass('active');
+        $('.menu__body').toggleClass('active');
+        $('.header__logo').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+
+})
+
+function ibg(){
+    let ibg=document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';}}}
+ibg();
